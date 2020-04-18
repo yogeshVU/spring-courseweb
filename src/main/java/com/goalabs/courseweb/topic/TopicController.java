d175d615602dfee2d7a31c041622c1646c6ed094
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2020-04-17 22:50:01 -0500

    update

--- src/main/java/com/goalabs/courseweb/topic/TopicController.java
+++ src/main/java/com/goalabs/courseweb/topic/TopicController.java
@@ -1,7 +1,9 @@
 package com.goalabs.courseweb.topic;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -9,8 +11,32 @@ import java.util.List;
 @RestController
 public class TopicController {
 
+    Logger logger = LoggerFactory.getLogger(TopicController.class);
+
+    final
+    TopicService topicService;
+
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+
     @RequestMapping("/topics")
-    public List<Topic> getTopic(){
-        return Arrays.asList(new Topic("spring","Spring Framework","Spring  tutorials"));
+    public List<Topic> getAllTopics(){
+//        return Arrays.asList(new Topic("spring","Spring Framework","Spring  tutorials"));
+        return topicService.getTopics();
+    }
+
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id){
+        logger.info("Request for topic id"+ id);
+        return topicService.getTopic(id);
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST,value = "/topics")
+    public void addTopic(@RequestBody Topic topic){
+        logger.info("adding a topic...");
+        topicService.addTopic(topic);
     }
 }
--- src/main/java/com/goalabs/courseweb/topic/TopicService.java
+++ src/main/java/com/goalabs/courseweb/topic/TopicService.java
@@ -1,4 +1,49 @@
 package com.goalabs.courseweb.topic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+@Service
 public class TopicService {
+    Logger logger = LoggerFactory.getLogger(TopicService.class);
+    private List<Topic> topics;
+
+
+    public TopicService(){
+        logger.info("Started the Topic Service....");
+        this.topics = new ArrayList<Topic>();
+
+        for (int i = 0; i < 4; i++) {
+            this.topics.add(new Topic("Java-"+Integer.toString(i),"Java course-"+Integer.toString(i), "Description of course "+Integer.toString(i) ));
+        }
+
+        for (int i = 0; i < 4; i++) {
+            this.topics.add(new Topic("Spring-"+Integer.toString(i),"Javascript course-"+Integer.toString(i), "Description of course "+Integer.toString(i) ));
+        }
+        for (int i = 0; i < 4; i++) {
+            this.topics.add(new Topic("JavaScript-"+Integer.toString(i),"Javascript course-"+Integer.toString(i), "Description of course "+Integer.toString(i) ));
+        }
+    }
+
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id){
+        return this.topics.stream().filter(a -> a.getId().equals(id)).findFirst().get();
+
+    }
+
+    public void addTopic(Topic topic){
+        this.topics.add(topic);
+    }
+//    public void setTopics(List<Topic> topics) {
+//        this.topics = topics;
+//    }
 }
